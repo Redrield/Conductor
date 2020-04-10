8f93e4849cc988d2c522f8aa35eed503730bd5fe
Redrield <redrield@gmail.com>
2020-04-10 02:31:16 -0400

    Update deps

--- src/ws.rs
+++ src/ws.rs
@@ -5,8 +5,10 @@ use actix_web::{HttpServer, web, HttpRequest, Responder, HttpResponse, App};
 use actix_web::body::Body;
 use resources::Resources;
 use std::borrow::Cow;
+use futures::future::Future;
 
-fn assets(path: web::Path<String>) -> impl Responder {
+fn assets(path: web::Path<String>) -> HttpResponse {
+
     let path = path.into_inner();
     match Resources::get(&path) {
         Some(content) => {
@@ -20,7 +22,7 @@ fn assets(path: web::Path<String>) -> impl Responder {
     }
 }
 
-fn index(_req: HttpRequest) -> impl Responder {
+fn index(_req: HttpRequest) -> HttpResponse {
     let contents = Resources::get("index.html").unwrap();
     let body: Body = match contents {
         Cow::Borrowed(bytes) => bytes.into(),
@@ -31,7 +33,7 @@ fn index(_req: HttpRequest) -> impl Responder {
         .body(body)
 }
 
-fn stdout(_req: HttpRequest) -> impl Responder {
+fn stdout(_req: HttpRequest) -> HttpResponse {
     let contents = Resources::get("stdout.html").unwrap();
     let body: Body = match contents {
         Cow::Borrowed(bytes) => bytes.into(),
@@ -46,6 +48,8 @@ pub fn launch_webserver() -> u16 {
     let (port_tx, port_rx) = mpsc::channel();
 
     thread::spawn(move || {
+        let sys = actix_rt::System::new("elm-ds-actix");
+        
         let server = HttpServer::new(|| {
             App::new().route("/", web::get().to(index))
                 .route("/stdout", web::get().to(stdout))
@@ -57,7 +61,10 @@ pub fn launch_webserver() -> u16 {
         let port = server.addrs().first().unwrap().port();
         port_tx.send(port).unwrap();
 
-        server.run().unwrap()
+
+        let server = server.run();
+        sys.run();
+        futures::executor::block_on(server.stop(true));
     });
 
     port_rx.recv().unwrap()
