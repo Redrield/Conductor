f4ebac9cfca99604ecd40df0bacee07d7c516ed6
Redrield <redrield@gmail.com>
2020-08-12 01:39:36 -0400

    Add tentative keybind implementation for macOS

--- src/keys.rs
+++ src/keys.rs
@@ -1,5 +1,17 @@
-
-mod arch;
-
 pub use self::arch::*;
 
+#[cfg(target_os = "linux")]
+#[path = "keys/linux.rs"]
+mod arch;
+
+#[cfg(target_os = "macos")]
+#[path = "keys/mac.rs"]
+mod arch;
+
+
+#[cfg(all(
+not(target_os = "linux"),
+not(target_os = "macos")
+))]
+#[path = "keys/default.rs"]
+mod arch;
---
+++ src/keys/mac.rs
@@ -0,0 +1,41 @@
+use std::sync::{Arc, RwLock};
+use crate::state::State;
+use actix::Addr;
+use crate::webserver::WebsocketHandler;
+use core_graphics::event::{KeyCode, CGKeyCode};
+use std::thread;
+use std::time::Duration;
+
+const kCGEventSourceStateCombinedSessionState: i32 = 0;
+
+
+#[link(name = "CoreGraphics", kind = "framework")]
+extern {
+    fn CGEventSourceKeyState(stateID: i32, key: CGKeyCode) -> bool;
+}
+
+pub fn bind_keys(state: Arc<RwLock<State>>, addr: Addr<WebsocketHandler>) -> bool {
+    unsafe {
+        thread::spawn(move || {
+            let mut return_pressed = false;
+            let mut space_pressed = false;
+
+            loop {
+                if CGEventSourceKeyState(kCGEventSourceStateCombinedSessionState, KeyCode::RETURN)
+                    && !return_pressed {
+                    println!("Disable the robot");
+                }
+
+                if CGEventSourceKeyState(kCGEventSourceStateCombinedSessionState, KeyCode::SPACE)
+                    && !space_pressed {
+                    println!("Estop the robot");
+                }
+
+                return_pressed = CGEventSourceKeyState(kCGEventSourceStateCombinedSessionState, KeyCode::RETURN);
+                space_pressed = CGEventSourceKeyState(kCGEventSourceStateCombinedSessionState, KeyCode::SPACE);
+                thread::sleep(Duration::from_millis(20));
+            }
+        });
+    }
+    true
+}
